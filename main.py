bf2de5e00cd378d3e7cd9c1118164ed7478bf001
rubenprofit <32101461+rubenprofit@users.noreply.github.com>
2023-06-13 22:22:47 -0400

    add sockets

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 def main():
+    import socket
+
+    assert url.startswith("http://")
+    url = url[len("https://"):]
+    host, path = url.split("/", 1)
+    path = "/" + path
+
+    s = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
+    )
+
+    s.connect((host, 80))
     return
 
 if __name__ == '__main__':
